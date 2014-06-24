748b0bfab85c5bc44e7d3876a90fc47b94071ccc
YANG, Shuh-Hua <shyang107@gmail.com>
2014-06-24 14:11:57 +0800

    print_cjk_unicode.py

--- print_unicode.py
+++ print_unicode.py
@@ -35,61 +35,6 @@ if len(sys.argv) > 1:
     else:
         word = sys.argv[1].lower()
 
-# if word != 0:
-# print_unicode_table(word)
+if word != 0:
+    print_unicode_table(word)
 
-print("\n\nThe CJK compatibility unicode table")
-
-
-def get_line(column, columns, fmt, head, line, split):
-    while column < columns:
-        line += fmt.format(head) + split
-        column += 1
-    return line
-
-
-def print_cjk_unicode_table():
-    columns = 12
-    width = 7
-    split = " "
-    column = 0
-    head1 = "char/uc"
-    head2 = "-" * width
-    fmt = "{0:^" + str(width) + "}"
-    line1 = ""
-    line2 = ""
-    line1 = get_line(column, columns, fmt, head1, line1, split)
-    line2 = get_line(column, columns, fmt, head2, line2, split)
-    print(line1)
-    print(line2)
-
-    code = ord(" ")
-    end = sys.maxunicode
-
-    column = 0
-    line1 = ""
-    line2 = ""
-    line3 = ""
-    rows = 10
-    row = 0
-    fmtc = "  {0} "
-    while code < end and row < rows:
-        c = chr(code)
-        name = unicodedata.name(c, "*** unknown ***")
-        cc = str(c).center(width, '*')
-        if "cjk" in name.lower():
-            if column < columns:
-                line1 += fmt.format(cc) + split
-                line2 += fmt.format(str(code)) + split
-                line3 += fmt.format(str(len(c))) + split
-                column += 1
-            else:
-                print(line1)
-                print(line3)
-                print(line2)
-                line1, line2, column = "", "", 0
-                row, line3 = row + 1, ""
-        code += 1
-
-
-print_cjk_unicode_table()
